440af5fb5dff2fc9a0c2ec937705d150380c32c0
stephanpesch <stephan.pesch1@gmail.com>
2023-04-12 21:05:18 +0200

    Reformatted LSTM algorithm for better readability

--- algorithms/lstm.py
+++ algorithms/lstm.py
@@ -1,96 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import tensorflow as tf
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+
 def lstm_algorithm(file, checked_columns, col_names):
-
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import pandas as pd
-    import tensorflow as tf
-    from tensorflow.python.keras.models import Sequential
-    from tensorflow.python.keras.layers import Dense
-    from tensorflow.python.keras.layers import LSTM
-    from sklearn.preprocessing import MinMaxScaler
-    from sklearn.metrics import mean_squared_error
-
-    df_long = pd.read_csv("csv_files/lstm/electricity-consumption-raw.csv", header=0, usecols=["datetime","consumption"], parse_dates=["datetime"])
+    # Reduce size of dataset so my computer can handle it
+    df_long = pd.read_csv("csv_files/lstm/electricity-consumption-raw.csv", header=0,
+                          usecols=["datetime", "consumption"], parse_dates=["datetime"])
     df_long["month"] = df_long["datetime"].dt.month
     df_long["year"] = df_long["datetime"].dt.year
     df_long["day"] = df_long["datetime"].dt.day
     df = df_long.groupby(["year", "month", "day"]).mean()
-    #df = df_long.head(1000)
-
-    # ---------------------------------------------------------------------------------
-
-    df_long = pd.read_csv("csv_files/lstm/electricity-consumption-raw.csv", header=0, usecols=["datetime","consumption"], parse_dates=["datetime"])
-    df_long["month"] = df_long["datetime"].dt.month
-    df_long["year"] = df_long["datetime"].dt.year
-    df_long["day"] = df_long["datetime"].dt.day
-    df = df_long.groupby(["year", "month", "day"]).mean()
-    #df = df_long.head(1000)
-
-    # ---------------------------------------------------------------------------------
-
-    df.tail()
-
-    # ---------------------------------------------------------------------------------
-
-    df.info()
-
-    # ---------------------------------------------------------------------------------
 
     # fix random seed for reproducibility
     tf.random.set_seed(7)
 
-    # ---------------------------------------------------------------------------------
-
     dataset = df.values
     dataset = dataset.astype('float32')
 
-    # ---------------------------------------------------------------------------------
-
-    # normalize the dataset
+    # normalize the dataset between 0 and 1
     scaler = MinMaxScaler(feature_range=(0, 1))
     dataset = scaler.fit_transform(dataset)
-
-    # ---------------------------------------------------------------------------------
 
     # split into train and test sets
     train_size = int(len(dataset) * 0.67)
     test_size = len(dataset) - train_size
-    train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+    train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
     print(len(train), len(test))
-
-    # ---------------------------------------------------------------------------------
-
-    # convert an array of values into a dataset matrix
-    def create_dataset(dataset, look_back=1):
-        dataX, dataY = [], []
-        for i in range(len(dataset)-look_back-1):
-            a = dataset[i:(i+look_back), 0]
-            dataX.append(a)
-            dataY.append(dataset[i + look_back, 0])
-        return np.array(dataX), np.array(dataY)
-
-    # ---------------------------------------------------------------------------------
 
     # reshape into X=t and Y=t+1
     look_back = 1
-    trainX, trainY = create_dataset(train, look_back)
-    testX, testY = create_dataset(test, look_back)
-
-    # ---------------------------------------------------------------------------------
-
-    trainY
-    
-    # ---------------------------------------------------------------------------------
-
-    trainX
-
-    # ---------------------------------------------------------------------------------
+    train_x, train_y = create_dataset(train, look_back)
+    test_x, test_y = create_dataset(test, look_back)
 
     # reshape input to be [samples, time steps, features]
-    trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-
-    # ---------------------------------------------------------------------------------
+    train_x = np.reshape(train_x, (train_x.shape[0], 1, train_x.shape[1]))
+    test_x = np.reshape(test_x, (test_x.shape[0], 1, test_x.shape[1]))
 
     # create and fit the LSTM network
     model = Sequential()
@@ -98,37 +49,43 @@ def lstm_algorithm(file, checked_columns, col_names):
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     # epochs changed from 100 to 15 only for test purposes 
-    model.fit(trainX, trainY, epochs=15, batch_size=1, verbose=2)
-
-    # ---------------------------------------------------------------------------------
+    model.fit(train_x, train_y, epochs=15, batch_size=1, verbose=2)
 
     # make predictions
-    trainPredict = model.predict(trainX)
-    testPredict = model.predict(testX)
+    train_predict = model.predict(train_x)
+    test_predict = model.predict(test_x)
     # invert predictions
-    trainPredict = scaler.inverse_transform(trainPredict)
-    trainY = scaler.inverse_transform([trainY])
-    testPredict = scaler.inverse_transform(testPredict)
-    testY = scaler.inverse_transform([testY])
+    train_predict = scaler.inverse_transform(train_predict)
+    train_y = scaler.inverse_transform([train_y])
+    test_predict = scaler.inverse_transform(test_predict)
+    test_y = scaler.inverse_transform([test_y])
     # calculate root mean squared error
-    trainScore = np.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-    print('Train Score: %.2f RMSE' % (trainScore))
-    testScore = np.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-    print('Test Score: %.2f RMSE' % (testScore))
-
-    # ---------------------------------------------------------------------------------
+    train_score = np.sqrt(mean_squared_error(train_y[0], train_predict[:, 0]))
+    print('Train Score: %.2f RMSE' % train_score)
+    test_score = np.sqrt(mean_squared_error(test_y[0], test_predict[:, 0]))
+    print('Test Score: %.2f RMSE' % test_score)
 
     # shift train predictions for plotting
-    trainPredictPlot = np.empty_like(dataset)
-    trainPredictPlot[:, :] = np.nan
-    trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+    train_predict_plot = np.empty_like(dataset)
+    train_predict_plot[:, :] = np.nan
+    train_predict_plot[look_back:len(train_predict) + look_back, :] = train_predict
     # shift test predictions for plotting
-    testPredictPlot = np.empty_like(dataset)
-    testPredictPlot[:, :] = np.nan
-    testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+    test_predict_plot = np.empty_like(dataset)
+    test_predict_plot[:, :] = np.nan
+    test_predict_plot[len(train_predict) + (look_back * 2) + 1:len(dataset) - 1, :] = test_predict
     # plot baseline and predictions
     plt.figure(figsize=(6, 5))
     plt.plot(scaler.inverse_transform(dataset))
-    plt.plot(trainPredictPlot)
-    plt.plot(testPredictPlot)
+    plt.plot(train_predict_plot)
+    plt.plot(test_predict_plot)
     plt.show(block=False)
+
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+    data_x, data_y = [], []
+    for i in range(len(dataset) - look_back - 1):
+        a = dataset[i:(i + look_back), 0]
+        data_x.append(a)
+        data_y.append(dataset[i + look_back, 0])
+    return np.array(data_x), np.array(data_y)
